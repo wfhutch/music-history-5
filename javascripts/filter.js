b17b3d7dbc428ede09d6c77cdd1a657cb5a4e535
Wayne Hutchinson <wfh2727@gmail.com>
2016-03-15 00:04:56 -0500

    Added message when no song match found for genre search.

--- javascripts/filter.js
+++ javascripts/filter.js
@@ -21,30 +21,20 @@ define(['jquery'], function($) {
         for(j=0; j<songs.songs.length; j++) {
           if (boxId[i] === songs.songs[j].genre) {
             genreSongs.push(songs.songs[j]);
-          }  
+          } 
         }
+        console.log("genreSongs", genreSongs);
       }
       
+      //Notify user if no songs matching that genre are found
       if (genreSongs.length === 0) {
-
-          require(['hbs!../templates/songs'], function(songTemplate) {
-          $("#content").html(songTemplate(songs));
-          $(".deleteButton").on("click", function() {
-          $(this).closest("div").remove();
-          });
-        });
+        $("#content").html("<h1>No songs found for that genre.</h1>");
         $('input[type=checkbox]').attr('checked',false);
-
       } else {
-
-      var genreSongsObject = {songs: genreSongs};
-
-      require(['hbs!../templates/songs'], function(songTemplate) {
-        $("#content").html(songTemplate(genreSongsObject));
-        $(".deleteButton").on("click", function() {
-        $(this).closest("div").remove();
+          var genreSongsObject = {songs: genreSongs};
+          require(['hbs!../templates/songs'], function(songTemplate) {
+          $("#content").html(songTemplate(genreSongsObject));
         });
-      });
           
         $('input[type=checkbox]').attr('checked',false);
       }  
